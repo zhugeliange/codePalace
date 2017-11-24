f62832db7393a6a6761710a6215909bd284ff4fc
Jay <jay.s@eyebuydirect.com>
2017-11-24 18:31:09 +0800

    test

---
+++ industry/index.php
@@ -0,0 +1,13 @@
+<?php
+	/**
+	 * parse xml to sql
+	 */
+
+// require_once(__DIR__ . "/phpSpreadsheet/IOFactory.php");
+
+// $result = \PhpOffice\PhpSpreadsheet\IOFactory::load('./industry.xlsx');
+
+// var_dump($result);
+// exit();
+
+phpinfo();
